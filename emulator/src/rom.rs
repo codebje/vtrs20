3c30e96ba15cad15e8dc0d2bb0585eed00f0042f
Byron Ellacott <code@bje.id.au>
2020-08-17 22:46:03 +1000

    emulate sst39 programming commands

--- emulator/src/rom.rs
+++ emulator/src/rom.rs
@@ -1,12 +1,30 @@
 use std::cell::RefCell;
+use std::thread;
+use std::time::{Duration, Instant};
 
-use crate::types::Peripheral;
+use crate::bus::Bus;
+use crate::types::*;
+
+#[derive(PartialEq, Copy, Clone)]
+enum Mode {
+    READ,
+    ID,
+    PROGRAM,
+    WRITING,
+    COMMAND1,
+    COMMAND2,
+    ERASE1,
+    ERASE2,
+    ERASE3,
+}
 
 pub struct ROM {
     start: u32,
     size: u32,
     bytes: RefCell<Vec<u8>>,
     is_masking: RefCell<bool>,
+    mode: RefCell<Mode>,
+    begun: RefCell<Instant>,
 }
 
 impl ROM {
@@ -16,6 +34,8 @@ impl ROM {
             size: contents.len() as u32,
             bytes: RefCell::new(contents),
             is_masking: RefCell::new(true),
+            mode: RefCell::new(Mode::READ),
+            begun: RefCell::new(Instant::now()),
         }
     }
 }
@@ -23,6 +43,17 @@ impl ROM {
 impl Peripheral for ROM {
     fn reset(&self) {
         *self.is_masking.borrow_mut() = true;
+    }
+
+    fn cycle(&self, _bus: &Bus) -> Option<Interrupt> {
+        // programming modes are exited after 10ms
+        if *self.mode.borrow() == Mode::WRITING {
+            if self.begun.borrow().elapsed() > Duration::from_millis(10) {
+                *self.mode.borrow_mut() = Mode::READ;
+            }
+        }
+
+        None
     }
 
     fn mem_read(&self, address: u32, m1: bool) -> Option<u8> {
@@ -39,12 +70,105 @@ impl Peripheral for ROM {
         }
 
         if addr >= self.start && addr <= self.start + self.size {
-            return Some(self.bytes.borrow()[(addr - self.start) as usize]);
+            let mut val = self.bytes.borrow()[(addr - self.start) as usize];
+
+            match *self.mode.borrow() {
+                Mode::READ => (),
+                Mode::ID => val = if addr & 1 == 0 { 0xbf } else { 0xd6 },
+                Mode::PROGRAM => (),
+                Mode::WRITING => val = (val ^ 0x80) & 0x80,
+                Mode::COMMAND1 => (),
+                Mode::COMMAND2 => (),
+                Mode::ERASE1 => (),
+                Mode::ERASE2 => (),
+                Mode::ERASE3 => (),
+            }
+
+            return Some(val);
         }
         None
     }
 
-    fn mem_write(&self, _address: u32, _data: u8) {
-        // TODO SST39xF0x0 Flash command sequences for write control
+    fn mem_write(&self, address: u32, data: u8) {
+        let mode = *self.mode.borrow();
+        match mode {
+            Mode::READ => {
+                if address == self.start + 0x5555 && data == 0xaa {
+                    *self.mode.borrow_mut() = Mode::COMMAND1;
+                }
+            }
+            Mode::ID => {
+                if address >= self.start && address <= self.start + self.size && data == 0xf0 {
+                    *self.mode.borrow_mut() = Mode::READ;
+                }
+            }
+            Mode::PROGRAM => {
+                if address >= self.start && address <= self.start + self.size {
+                    //println!("Write byte {:04x} to {:05x}", data, address);
+                    self.bytes.borrow_mut()[(address - self.start) as usize] = data;
+                    *self.mode.borrow_mut() = Mode::WRITING;
+                    *self.begun.borrow_mut() = Instant::now();
+                }
+            }
+            Mode::WRITING =>
+            //(), // nothing is accepted during programming
+            {
+                if address >= self.start && address <= self.start + self.size {
+                    println!("Attempt to write byte {:04x} to {:05x} during WRITING", data, address);
+                    let one_ms = Duration::from_millis(100000);
+                    thread::sleep(one_ms);
+                }
+            }
+            Mode::COMMAND1 => {
+                if address == self.start + 0x2aaa && data == 0x55 {
+                    *self.mode.borrow_mut() = Mode::COMMAND2;
+                } else if address >= self.start && address <= self.start + self.size {
+                    println!("COMMAND1 exited with byte {:04x} to {:05x}", data, address);
+                    *self.mode.borrow_mut() = Mode::READ;
+                }
+            }
+            Mode::COMMAND2 => {
+                if address == self.start + 0x5555 && data == 0xA0 {
+                    *self.mode.borrow_mut() = Mode::PROGRAM;
+                } else if address == self.start + 0x5555 && data == 0x80 {
+                    *self.mode.borrow_mut() = Mode::ERASE1;
+                } else if address == self.start + 0x5555 && data == 0x90 {
+                    *self.mode.borrow_mut() = Mode::ID;
+                } else if address >= self.start && address <= self.start + self.size {
+                    println!("COMMAND2 exited with byte {:04x} to {:05x}", data, address);
+                    *self.mode.borrow_mut() = Mode::READ;
+                }
+            }
+            Mode::ERASE1 => {
+                if address == self.start + 0x5555 && data == 0xaa {
+                    *self.mode.borrow_mut() = Mode::ERASE2;
+                } else if address >= self.start && address <= self.start + self.size {
+                    *self.mode.borrow_mut() = Mode::READ;
+                }
+            }
+            Mode::ERASE2 => {
+                if address == self.start + 0x2aaa && data == 0x55 {
+                    *self.mode.borrow_mut() = Mode::ERASE3;
+                } else if address >= self.start && address <= self.start + self.size {
+                    *self.mode.borrow_mut() = Mode::READ;
+                }
+            }
+            Mode::ERASE3 => {
+                if address == self.start + 0x5555 && data == 0x10 {
+                    // erase the lot, pow!
+                    self.bytes.borrow_mut().iter_mut().map(|x| *x = 0xff).count();
+                    *self.mode.borrow_mut() = Mode::WRITING;
+                } else if address >= self.start && address <= self.start + self.size && data == 0x30 {
+                    let mut bytes = self.bytes.borrow_mut();
+                    let x = (address - self.start) as usize;
+                    for addr in (x & !0xfff)..(x & !0xfff) + 0x1000 {
+                        bytes[addr] = 0xff;
+                    }
+                    *self.mode.borrow_mut() = Mode::WRITING;
+                } else if address >= self.start && address <= self.start + self.size {
+                    *self.mode.borrow_mut() = Mode::READ;
+                }
+            }
+        }
     }
 }
